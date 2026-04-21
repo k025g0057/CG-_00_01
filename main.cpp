1472ba1ba19010f307d2a9bdf9a29db3c7f2eabf
hayashi teruto <k025g0057@g.neec.ac.jp>
2026-04-21 16:55:21 +0900

    CG_00_06_end

--- main.cpp
+++ main.cpp
@@ -7,12 +7,49 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <cassert>
+#include <dbghelp.h>
+#include <strsafe.h>
 
 //libのリンク-- -
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "Dbghelp.lib")
 
 
+
+// --- スライド「CrashHandlerの登録」: 関数定義 ---
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
+    // 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下に出力
+    SYSTEMTIME time;
+    GetLocalTime(&time);
+
+    wchar_t filePath[MAX_PATH] = { 0 };
+    CreateDirectory(L"./Dumps", nullptr);
+
+    // StringCchPrintfWを利用してファイルパスを作成
+    StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d-%02d%02d-%02d%02d.dmp",
+        time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute);
+
+    HANDLE dumpFileHandle = CreateFile(filePath, GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+
+    // processId (このexeのId) とクラッシュ (例外) の発生したthreadIdを取得
+    DWORD processId = GetCurrentProcessId();
+    DWORD threadId = GetCurrentThreadId();
+
+    // 設定情報を入力
+    MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{ 0 };
+    minidumpInformation.ThreadId = threadId;
+    minidumpInformation.ExceptionPointers = exception;
+    minidumpInformation.ClientPointers = TRUE;
+
+    // Dumpを出力。MiniDumpNormalは最低限の情報を出力するフラグ
+    MiniDumpWriteDump(GetCurrentProcess(), processId, dumpFileHandle, MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
+
+    // 他に関連づけられているSEH例外ハンドラがあれば実行。通常はプロセスを終了する
+    return EXCEPTION_EXECUTE_HANDLER;
+
+}
 
 
 // ファイルと出力ウィンドウ両方に出す関数 ---
@@ -54,6 +91,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+    
+    SetUnhandledExceptionFilter(ExportDump);
+
+    uint32_t* p = nullptr; // 故意にクラッシュさせるためのポインた
+    *p = 100; // 故意にクラッシュさせるためのコード
 
     // logsディレクトリを用意
     std::filesystem::create_directory("logs");
